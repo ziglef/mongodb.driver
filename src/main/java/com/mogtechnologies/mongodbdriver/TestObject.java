92819b1fc6cd4565e6ebd3ea7de414f140432f90
Rui Grandão <ruigrandaorocha@gmail.com>
2016-02-23 17:26:27 +0000

    Removed TestObject and added SimpleDocument. It serves as a test object for morphia features.

--- src/main/java/com/mogtechnologies/mongodbdriver/TestObject.java
+++
@@ -1,7 +0,0 @@
-package com.mogtechnologies.mongodbdriver;
-
-/**
- * Created by rgrandao on 22/02/2016.
- */
-public class TestObject {
-}
---
+++ src/main/java/com/mogtechnologies/mongodbdriver/database/models/SimpleDocument.java
@@ -0,0 +1,10 @@
+package com.mogtechnologies.mongodbdriver.database.models;
+
+import org.mongodb.morphia.annotations.Entity;
+
+/**
+ * Created by rgrandao on 23/02/2016.
+ */
+@Entity
+public class SimpleDocument {
+}
